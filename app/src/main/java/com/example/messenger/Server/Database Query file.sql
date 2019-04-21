9e8961e92f7ad558124544c895f66972c61306d1
Laptop <lanehacker7294@yahoo.com>
2019-04-22 02:53:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/messenger/Server/Database Query file.sql
@@ -0,0 +1,27 @@
+create database MessagingApp
+use MessagingApp
+
+create table account(
+	AccountID varchar(10) primary key,
+    Username varchar(30),
+    Password varchar(20)
+)
+
+create table message(
+	MessageID varchar(10) primary key,
+	SenderID varchar(10),
+    ReceiverID varchar(10),
+    AccountID varchar(10) ,
+    Content text,
+    constraint FK_account foreign key (AccountID) references account(AccountID)  
+)
+
+create table friendlist(
+	FriendListID varchar(10) primary key,
+    AccountID varchar(10),
+    FriendID varchar(10),
+    FriendUsername varchar(30),
+    constraint FK_friendlist foreign key(AccountID) references account(AccountID)
+)
+    
+    
