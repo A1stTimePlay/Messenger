53833bdbae918e769e41ff990bf97d4e6b6f64ba
Nguyen Xuan An <lanehacker7294@yahoo.com>
2019-05-31 18:58:05 +0700

    small fix to code

--- app/src/main/java/com/example/messenger/SplashScreen.java
+++ app/src/main/java/com/example/messenger/SplashScreen.java
@@ -32,6 +32,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 5500);
+        }, 2500);
     }
 }
